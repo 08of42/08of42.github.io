8d5bf44e68a32195ecc486b8e8ccf7b6ec4cc94f
Bowa1000 <Bowa1000@gmail.com>
2026-01-30 20:43:55 -0600

    attribution and audio fix

--- tv/script.js
+++ tv/script.js
@@ -1,28 +1,23 @@
-
 function image(src){
 	const img=new Image();
 	img.src=src;
 	return img;
 }
-function audio(src){
-	const audio=new Audio(src);
-	audio.loop=true;
-	return audio;
-}
 const channels=[
-	{video:image("channels/black_noise.png"      ),audio:audio("channels/white_noise.ogg"  ),frame_time:1,                },
-	{video:image("channels/block_game.png"       ),audio:audio("channels/blocky_game.ogg"  ),frame_time:6,                },
-	{video:image("channels/colour_test.png"      ),audio:audio("channels/colour_test.ogg"  ),frame_time:1,                },
-	{video:image("channels/dance_music.png"      ),audio:audio("channels/dance_music.ogg"  ),frame_time:2,interpolate:true},
-	{video:image("channels/heart_screensaver.png"),audio:null                               ,frame_time:1,                },
-	{video:image("channels/herobrine.png"        ),audio:audio("channels/colour_test.ogg"  ),frame_time:3,                },
-	{video:image("channels/ocean_sunset.png"     ),audio:audio("channels/ocean_sunset.ogg" ),frame_time:4,interpolate:true},
-	{video:image("channels/pong.png"             ),audio:audio("channels/retro_song.ogg"   ),frame_time:2,                },
-	{video:image("channels/rip_blizzard.png"     ),audio:audio("channels/chirp_song.ogg"   ),frame_time:4,                },
-	{video:image("channels/silly_face.png"       ),audio:null                               ,frame_time:5,                },
-	{video:image("channels/villager_news.png"    ),audio:audio("channels/villager_news.ogg"),frame_time:5,                },
-	{video:image("channels/white_noise.png"      ),audio:audio("channels/white_noise.ogg"  ),frame_time:1,                },
+	{video:image("channels/black_noise.png"      ),audio:new Audio("channels/white_noise.ogg"  ),frame_time:1,                },
+	{video:image("channels/block_game.png"       ),audio:new Audio("channels/blocky_game.ogg"  ),frame_time:6,                },
+	{video:image("channels/colour_test.png"      ),audio:new Audio("channels/colour_test.ogg"  ),frame_time:1,                },
+	{video:image("channels/dance_music.png"      ),audio:new Audio("channels/dance_music.ogg"  ),frame_time:2,interpolate:true},
+	{video:image("channels/heart_screensaver.png"),audio:null                                   ,frame_time:1,                },
+	{video:image("channels/herobrine.png"        ),audio:new Audio("channels/colour_test.ogg"  ),frame_time:3,                },
+	{video:image("channels/ocean_sunset.png"     ),audio:new Audio("channels/ocean_sunset.ogg" ),frame_time:4,interpolate:true},
+	{video:image("channels/pong.png"             ),audio:new Audio("channels/retro_song.ogg"   ),frame_time:2,                },
+	{video:image("channels/rip_blizzard.png"     ),audio:new Audio("channels/chirp_song.ogg"   ),frame_time:4,                },
+	{video:image("channels/silly_face.png"       ),audio:null                                   ,frame_time:5,                },
+	{video:image("channels/villager_news.png"    ),audio:new Audio("channels/villager_news.ogg"),frame_time:5,                },
+	{video:image("channels/white_noise.png"      ),audio:new Audio("channels/white_noise.ogg"  ),frame_time:1,                },
 ];
+for(const{audio}of channels)if(audio)audio.loop=true;
 const noise={video:image("channels/white_noise.png"),audio:new Audio("channels/white_noise.ogg"),frame_time:1,};
 
 const ctx=document.createElement("canvas").getContext("2d",{antialias:false});
